6d4a2b22b25d54e220c196a9783069ce27b88e85
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-08 20:46:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["zero", "one", "two", "three"]
-  array.pop
+  array.pop("three") 
 end
 
 def remove_element_from_start_of_array(array)
