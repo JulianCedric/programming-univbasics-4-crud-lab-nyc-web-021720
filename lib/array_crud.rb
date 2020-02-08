0582cb91e7ea1763e7d9ae29d09acc7b0802165d
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-08 20:37:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["zero", "one", "two", "three"]
-  "four" <<
+  array = ["zero", "one", "two", "three"]
+  array << "four"
 end
 
 def add_element_to_start_of_array(array, element)
